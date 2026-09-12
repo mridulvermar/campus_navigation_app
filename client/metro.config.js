337ffddf6c02e62a6953b82df3cfa08a336edec3
mridulvermar <mridultechno2003@gmail.com>
2026-09-12 21:05:57 +0530

    fix(metro): configure client watchFolders and resolver paths to prevent ENOENT on root node_modules

--- client/metro.config.js
+++ client/metro.config.js
@@ -1,5 +1,11 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
+
+config.watchFolders = [__dirname];
+config.resolver.nodeModulesPaths = [
+  path.resolve(__dirname, 'node_modules')
+];
 
 module.exports = config;
